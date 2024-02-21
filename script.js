42d522b79e772f469ec30717fd41eeb34c064293
Nathan Adams <47441281+nadamspueblo@users.noreply.github.com>
2024-02-21 15:54:00 +0000

    Adding functionality

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
 
-let deg = 0;
+
 
 function init() {
   createElement("button1", "button");
   setText("button1", "Submit");
   setX("button1", 200);
   setY("button1", 300);
+  createRect(100, 200, 400, 75, "blue", "blueRect");
+  rotate("blueRect", 45);
+  createCircle(600, 200, 50, "red");
+  setProperty("blueRect", "border-radius", "4px");
+  console.log(getWindowWidth());
 }
 
 function mainLoop() {
-  rotate("test", deg);
-  deg += 10;
+  move("blueRect", 5);
 }
 
 init();
