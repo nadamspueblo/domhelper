46ef3458d7b97dfcd78b7fb810d8662b9e6ddfd1
nadamspueblo <47441281+nadamspueblo@users.noreply.github.com>
2024-02-22 09:36:32 -0700

    Completed isTouching function

--- domhelper.js
+++ domhelper.js
@@ -321,8 +321,10 @@ function isTouching(id1, id2) {
     return false;
   }
 
+  let rect1 = e1.getBoundingClientRect();
+  let rect2 = e2.getBoundingClientRect();
 
-
+  return rect1.right > rect2.left && rect1.left < rect2.right && rect1.bottom > rect2.top && rect1.top < rect2.bottom;
 }
 
 /* *************** Animation ******************* */
