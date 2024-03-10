2d51e40d4bfa93d38c7cb5492d28aef4ca581892
nadamspueblo <47441281+nadamspueblo@users.noreply.github.com>
2024-03-10 08:15:21 -0700

    Updated example script

--- example-script.js
+++ example-script.js
@@ -2,7 +2,7 @@ createElement("h1", "heading");
 setWidth(getWidth(), "heading");
 setText("Welcome", "heading");
 setColor("orange", "heading");
-setProperty("text-align", "center", "heading");
+setStyle("text-align", "center", "heading");
 
 createButton("start-button", "Start");
 setX(getWidth() / 2 - getWidth("start-button") / 2, "start-button");
@@ -10,5 +10,6 @@ setY(getHeight() / 2 - getHeight("start-button") / 2, "start-button");
 addClickEvent("start-button", start);
 
 function start() {
+  createCircle(getRandomInt(0, getWidth()), getRandomInt(0, getHeight()), 50, getRandomColor());
   console.log("Woohoo");
 }
