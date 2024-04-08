83ec400c77484a0e94f8a22ff461c70906b1ccf1
Nathan Adams <47441281+nadamspueblo@users.noreply.github.com>
2024-04-08 15:52:21 +0000

    Example updated

--- example-script.js
+++ example-script.js
@@ -1,15 +1,79 @@
-createElement("h1", "heading");
-setWidth(getWidth(), "heading");
-setText("Welcome", "heading");
-setColor("orange", "heading");
-setStyle("text-align", "center", "heading");
+let speed = 2;
+let leftPressed = false;
+let rightPressed = false;
 
-createButton("start-button", "Start");
-setX(getWidth() / 2 - getWidth("start-button") / 2, "start-button");
-setY(getHeight() / 2 - getHeight("start-button") / 2, "start-button");
-addClickEvent("start-button", start);
-
-function start() {
-  createCircle(getRandomInt(0, getWidth()), getRandomInt(0, getHeight()), 50, getRandomColor());
-  console.log("Woohoo");
+function init() {
+  showControls();
+  createCircle(getWidth() / 2, getHeight() - 200, 20, "red", "ball");
+  createRect(getWidth() / 2 - 100, getHeight() - 100, 200, 40, "blue", "paddle");
+  setRotation(getRandomInt(-110, -70), "ball");
+  addEventListener("keydown", keyDown);
+  addEventListener("keyup", keyUp);
 }
+
+function mainLoop() {
+  animateBall();
+  animatePaddle();
+  if (isTouching("ball", "paddle")) {
+    let angle = getRotation("ball");
+    angle = -angle;
+    setRotation(angle, "ball");
+  }
+}
+
+function reset() {
+  setRotation(getRandomInt(-110, -70), "ball");
+  setX(getWidth() / 2, "ball");
+  setY(getHeight() / 2, "ball");
+}
+
+function animateBall() {
+  move(speed, "ball");
+
+  if (getX("ball") >= getWidth() - 20 || getX("ball") <= 20) {
+    let angle = getRotation("ball");
+    angle = (180 - angle);
+    setRotation(angle, "ball");
+  }
+  else if (getY("ball") >= getHeight() - 20 || getY("ball") <= 20) {
+    let angle = getRotation("ball");
+    angle = -angle;
+    setRotation(angle, "ball");
+  }
+}
+
+function animatePaddle() {
+  if (leftPressed) {
+    changeXBy(-10, "paddle");
+    if (getX("paddle") < 0){
+      setX(0, "paddle");
+    }
+  }
+  if (rightPressed) {
+    changeXBy(10, "paddle");
+    if (getX("paddle") > getWidth() - getWidth("paddle")){
+      setX(getWidth() - getWidth("paddle"), "paddle");
+    }
+  }
+}
+
+function keyDown(event) {
+  if (event.code == "ArrowLeft") {
+    leftPressed = true;
+  }
+  if (event.code == "ArrowRight") {
+    rightPressed = true;
+  }
+  
+}
+
+function keyUp(event) {
+  if (event.code == "ArrowLeft") {
+    leftPressed = false;
+  }
+  if (event.code == "ArrowRight") {
+    rightPressed = false;
+  }
+}
+
+init();
